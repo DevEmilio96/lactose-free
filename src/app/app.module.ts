2683d8211aa849280a047a2447c00f689fc11fe2
DevEmilio <minima998@gmail.com>
2019-02-01 16:23:51 +0100

    Login e Registrazione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
+import { MyAppComponent } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Firebase } from '@ionic-native/firebase';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireStorageModule, AngularFireStorage } from "angularfire2/storage";
+import { HttpClientModule } from '@angular/common/http';
 
+import { HeaderEsComponent } from '../components/header-es/header-es';
+import { FooterEsComponent } from '../components/footer-es/footer-es'
+import { LoginPage } from '../pages/login/login';
+import { RegistrationPageComponent } from '../pages/registration/registration';
+import { AccountService } from '../providers/service/accountService';
+import { ENV } from '../config/env';
 @NgModule({
   declarations: [
-    MyApp,
+    MyAppComponent,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    RegistrationPageComponent,
+    HeaderEsComponent,
+    FooterEsComponent,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(ENV.firebase),
+    IonicModule.forRoot(MyAppComponent),
+    AngularFireStorageModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    MyAppComponent,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    RegistrationPageComponent
   ],
   providers: [
+    AngularFireAuth,
+    AccountService,
+    AngularFireStorage,
     StatusBar,
     SplashScreen,
+    Firebase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
